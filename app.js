e61545a590cb8889a4d164fefd94bb664526584f
Chris Beavis <cbeavis@theframeworks.com>
2024-10-07 11:23:14 +0100

    no message

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 // Initialize Express application
 const app = express();
