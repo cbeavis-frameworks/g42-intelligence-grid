b7ad637a1bcb43a77ef1e08810af4dc5b31e6c38
Chris Beavis <cbeavis@theframeworks.com>
2024-10-07 11:36:32 +0100

    no message

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve the main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Endpoint to serve the data
